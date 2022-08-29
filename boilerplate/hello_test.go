807cfa934549086530949b40bed78d44b5f36fd8
sters <sters@users.noreply.github.com>
2022-08-29 22:16:01 +0900

    fix

--- boilerplate/hello_test.go
+++ boilerplate/hello_test.go
@@ -3,6 +3,8 @@ package boilerplate
 import "testing"
 
 func TestHello(t *testing.T) {
+	t.Parallel()
+
 	if want, got := "Hello", Hello(); want != got {
 		t.Errorf("want = %s, god = %s", want, got)
 	}
