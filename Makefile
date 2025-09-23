0ef31d6d2601cceb27b70e9417356a4fc3147b22
sters <sters@users.noreply.github.com>
2025-09-23 14:08:30 +0900

    Add version embedding support for go install

--- Makefile
+++ Makefile
@@ -2,9 +2,15 @@
 export GOBIN := $(PWD)/bin
 export PATH := $(GOBIN):$(PATH)
 
+# Version information
+VERSION ?= dev
+COMMIT := $(shell git rev-parse --short HEAD 2>/dev/null || echo "unknown")
+DATE := $(shell date -u +"%Y-%m-%dT%H:%M:%SZ")
+LDFLAGS := -X main.version=$(VERSION) -X main.commit=$(COMMIT) -X main.date=$(DATE)
+
 .PHONY: run
 run:
-	go run main.go $(ARGS)
+	go run -ldflags "$(LDFLAGS)" . $(ARGS)
 
 .PHONY: lint
 lint:
@@ -25,3 +31,7 @@ cover:
 .PHONY: tidy
 tidy:
 	go mod tidy
+
+.PHONY: build
+build:
+	go build -ldflags "$(LDFLAGS)" -o bin/app .
--- main.go
+++ main.go
@@ -1,11 +1,88 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"runtime"
+	"runtime/debug"
+	"time"
 
 	"github.com/sters/go-project-boilerplate/boilerplate"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
+func getVersion() string {
+	if version != "dev" {
+		return version
+	}
+
+	// go installでビルドされた場合、debug.BuildInfoから情報を取得
+	if info, ok := debug.ReadBuildInfo(); ok {
+		if info.Main.Version != "" && info.Main.Version != "(devel)" {
+			return info.Main.Version
+		}
+	}
+	return version
+}
+
+func getCommit() string {
+	if commit != "none" {
+		return commit
+	}
+
+	// go installでビルドされた場合、VCS情報から取得
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				if len(setting.Value) > 7 {
+					return setting.Value[:7] // 短縮形にする
+				}
+				return setting.Value
+			}
+		}
+	}
+	return commit
+}
+
+func getDate() string {
+	if date != "unknown" {
+		return date
+	}
+
+	// go installでビルドされた場合、VCS情報から取得
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.time" {
+				if t, err := time.Parse(time.RFC3339, setting.Value); err == nil {
+					return t.UTC().Format("2006-01-02T15:04:05Z")
+				}
+				return setting.Value
+			}
+		}
+	}
+	return date
+}
+
 func main() {
+	var showVersion bool
+	flag.BoolVar(&showVersion, "version", false, "show version information")
+	flag.BoolVar(&showVersion, "v", false, "show version information (short)")
+	flag.Parse()
+
+	if showVersion {
+		fmt.Printf("Version:    %s\n", getVersion())
+		fmt.Printf("Commit:     %s\n", getCommit())
+		fmt.Printf("Built:      %s\n", getDate())
+		fmt.Printf("Go version: %s\n", runtime.Version())
+		fmt.Printf("OS/Arch:    %s/%s\n", runtime.GOOS, runtime.GOARCH)
+		os.Exit(0)
+	}
+
 	fmt.Printf("%s World!", boilerplate.Hello())
 }
